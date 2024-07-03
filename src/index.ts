612049f188eccf65f97ae1aa82d5d5d63dcf72ca
KitoMC <95675413+KitoMine@users.noreply.github.com>
2024-07-03 21:00:08 +0700

    🎨 Upd: Make color

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,28 @@ import routes from "./routes";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import config from "./config";
+import chalk from "chalk";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+const now = new Date();
+const hours = now.getHours();
+const minutes = now.getMinutes();
+const seconds = now.getSeconds();
+
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log(`[${chalk.cyan("START")}] ${chalk.gray.bold(`${hours}:${minutes}:${seconds}`)} Connected to MongoDB`);
+    app.listen(config.PORT, () => {
+      console.log(`[${chalk.cyan("START")}] ${chalk.gray.bold(`${hours}:${minutes}:${seconds}`)} Server run on port ${config.PORT}`);
+    });
   })
   .catch((err) => {
-    console.error("Failed to connect to MongoDB", err);
+    console.error(`[${chalk.redBright("ERROR")}] ${chalk.gray.bold(`${hours}:${minutes}:${seconds}`)} Failed to connect to MongoDB \n`, err);
   });
 
 app.use("/", routes);
-
-app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`);
-});
